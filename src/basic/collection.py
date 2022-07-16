65662e4f7926cece6e622c036776db98c01bc175
iambudi <iambudi@outlook.com>
2022-07-16 08:56:14 +0700

    UPD: Collection updated with comment

--- src/basic/collection.py
+++ src/basic/collection.py
@@ -20,18 +20,26 @@ User = NamedTuple("User", [("id", int), ("name", str)])
 user = User(1, "admin")
 print("Named Tuple", f"{user.name} has id {user[0]}")
 
-# MUTABLE
-# 1. List (Array)
-# =========================================================================================
+"""
+MUTABLE
+1. List (Array)
+=========================================================================================
+"""
 numbers: List[int] = [1, 3, 4, 5, 6, 7]
-numbers.append(10)
-numbers.pop(0)
-print("Number after pop should not contain 1", numbers)
-print("Index of value 5 is", numbers.index(5))
-print("Slice all numbers", numbers[:])
+
+# Slicing by element index [start:stop:step]
+print("Slice all numbers", numbers[:])  # exclude first element
+print("Slice by two steps", numbers[0:5:2])  # start from index 0 till 5 => [1,4,6]
 print("Slice exclude last two", numbers[:-2])
 print("Slice first two", numbers[:2])
 print("Slice with same index:index return nothing", numbers[2:2])
+
+# Add and remove element
+numbers.append(10)
+numbers.pop()  # pop last element in the list
+numbers.pop(0)  # pop by element index
+print("Number after pop should not contain 1", numbers)
+print("Index of value 5 is", numbers.index(5))
 
 # different ways to clear list
 numbers.clear()
@@ -41,25 +49,27 @@ del numbers[:]  # del range start to end
 print("Numbers after clear", numbers)
 
 """
-2. Set: unordered, no duplicate
-initializing dont use empty {} it belongs to dict
+2. Set: unordered, no duplicate (unique collection)
 =========================================================================================
 """
+# 2 ways to init set
 mySet: Set[int] = set()
-# another way
 mySet = set[int]()  # use () to differentiate with generic type alias
+# mySet = {} <-- this does not work, it's not dict
 print("Empty set", type(mySet))
+
 mySet = {1, 2, 2, 3, 3, 3}
 mySet = set([1, 2, 3, 4, 5, 5, 5, 5, 5])
 
 mySet.add(100)
 mySet.add(200)
 print("Set unique:", mySet)
+
 for i, s in enumerate(mySet):
     print(f"Set {i}. {s}")
 
 """
-4. Dictionary
+3. Dictionary
 =========================================================================================
 """
 userDict = {"id": 1, "name": "Admin"}
@@ -76,7 +86,7 @@ for key, val in userDict.items():
 def register(users: list[User]):
     """
     collection can be used as typehint.
-    here we use User named tuple User
+    here we use `User` as a named tuple
     """
     for usr in users:
         print(f"Register user {usr.name} success")
@@ -87,19 +97,21 @@ user2 = User(1, "Python")
 register([user1, user2])
 
 """
-# 5. Array
+# 4. Array
 =========================================================================================
 Python does not have array as primitive data structure
-It belongs to NumPy module.
+The primitive one belongs to NumPy module.
 + compact and storage efficient.
 + behave very much like lists,
-- objects stored is constrained (same type code, not heterogeneous)
+- objects stored is constrained (same typecode, not heterogeneous)
 ========================================================================================
-# typecode: i = signed int, I unsigned, f float, d double, b = char
-# complete list of type code
-# refer https://docs.python.org/3/library/array.html
+typecode: i = signed int, I unsigned, f float, d double, b = char
+complete list of typecode
+refer https://docs.python.org/3/library/array.html
+========================================================================================
 """
-
-ary = array("i", [1, 2, 3, 4])  # this would be invalid [1,2,3,"4"])
-print("Type of ary is", type(ary))
-print(ary, array("u", "hello \u2641"), array("d", [1.0, 2.0]))
+# first arg is typecode
+ary_int = array("i", [1, 2, 3, 4])  # this would be invalid [1,2,3,"4"])
+print("Type of ary is", type(ary_int))
+# another typecode
+print(ary_int, array("u", "hello \u2641"), array("d", [1.0, 2.0]))
